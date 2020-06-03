6484898e57ae7a0750c5ecd87d9706bec22a40a9
iporat08 <51414337+iporat08@users.noreply.github.com>
2020-06-03 13:52:28 +0300

    finalley - basic loaction works

--- app/src/main/java/com/idoporat/honeyimhome/LocationTracker.java
+++ app/src/main/java/com/idoporat/honeyimhome/LocationTracker.java
@@ -1,27 +1,39 @@
 package com.idoporat.honeyimhome;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
-import android.provider.Settings;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 
 public class LocationTracker {
 
     private Context context;
-    private BroadcastReceiver br;
+//    private BroadcastReceiver br;
+    public LocationInfo locationInfo;
     private boolean locationEnabled;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     private final static String TAG = "lack of permission";
     private final static String PERMISSION_ERROR_MSG = "app dose'nt have location permission";
@@ -29,32 +41,38 @@ public class LocationTracker {
 
     LocationTracker(Context context){
         this.context = context;
-        br = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if(LocationResult.hasResult(intent)){}
-                Intent location = new Intent();
-                intent.setAction("location_update");
-//                intent.putExtra()
-
-            }
-        };
+        mFusedLocationClient = getFusedLocationProviderClient(context);
+        locationInfo = new LocationInfo();
+//        br = new BroadcastReceiver() {
+//            @Override
+//            public void onReceive(Context context, Intent intent) {
+//                if(LocationResult.hasResult(intent)){}
+//                Intent location = new Intent();
+//                intent.setAction("location_update");
+////                intent.putExtra()
+//
+//            }
+//        };
     }
 
     public boolean startTracking(){
         // todo - will start tracking the location and send a "started" broadcast intent
 
-        boolean hasPermission = ActivityCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_FINE_LOCATION) ==
-                PackageManager.PERMISSION_GRANTED;
-        if(hasPermission) {
+        if(hasPermissions()) {
             if(isLocationEnabled()){
-               return true;
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(
+                        new OnCompleteListener<Location>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Location> task) {
+                                requestNewLocationData();
+                            }
+                        });
+                return true;
             }
         }
-        else {
-            Log.d(TAG, PERMISSION_ERROR_MSG);
-        }
+
+
+        Log.d(TAG, PERMISSION_ERROR_MSG);
         return false;
 
         // todo - The LocationTracker class should assume it has anything it needs in order to run.
@@ -65,8 +83,45 @@ public class LocationTracker {
         //  the UI).
     }
 
+    public void onLocationChanged(Location location) {
+        locationInfo.setAccuracy(location.getAccuracy());
+        locationInfo.setLatitude(location.getLatitude());
+        locationInfo.setLongitude(location.getLongitude());
+
+        Intent locationIntent = new Intent();
+        locationIntent.setAction(context.getString(R.string.new_location));
+        locationIntent.putExtra(context.getString(R.string.location_intent_key), location);
+        context.sendBroadcast(locationIntent);
+    }
+
     public void stopTracking(){
         //todo - will stop tracking and send a "stopped" broadcast intent
+    }
+
+    private boolean hasPermissions(){
+        return ActivityCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestNewLocationData(){
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(0); //todo maybe more? like 5000?
+        locationRequest.setFastestInterval(0);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        mFusedLocationClient.requestLocationUpdates(locationRequest, new LocationCallback(){
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
+        }
+
+    public LocationInfo getLocationInfo(){
+        return locationInfo;
     }
 
     private boolean isLocationEnabled(){
@@ -83,37 +138,13 @@ public class LocationTracker {
         } catch(Exception ex) {}
 
         if(!gps_enabled && !network_enabled) {
-            // notify user
-//            new AlertDialog.Builder(context)
-//                    .setMessage(R.string.gps_network_not_enabled)
-//                    .setPositiveButton(R.string.open_location_settings,
-//                                        new DialogInterface.OnClickListener() {
-//                        @Override
-//                        public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-//                            context.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-//                        }
-//                    })
-//                            .setNegativeButton(R.string.Cancel,null)
-//                            .show();
-            CharSequence text = "make sure location and network are enabled";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
+            Intent noLocationIntent = new Intent();
+            noLocationIntent.setAction("no_location");
+            context.sendBroadcast(noLocationIntent);
             return false;
         }
         else{
             return true;
         }
-//        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-//                .addLocationRequest(new LocationRequest()
-//                        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-//                        .setInterval(0)); //todo???
-
-
-
-
-
-
-
     }
 }
--- app/src/main/java/com/idoporat/honeyimhome/MainActivity.java
+++ app/src/main/java/com/idoporat/honeyimhome/MainActivity.java
@@ -5,21 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private LocationTracker locationTracker;
+//    private LocationInfo locationInfo;
     private BroadcastReceiver br;
     private final int REQUEST_CODE_PERMISSION_LOCATION = 1;
     private Button trackingButton;
+    private TextView latitudeView;
+    private TextView longitudeView;
+    private TextView accuracyView;
     private boolean tracking;
 
     @Override
@@ -29,14 +39,15 @@ public class MainActivity extends AppCompatActivity {
 
         tracking = false;
         trackingButton = findViewById(R.id.Start_tracking_button);
+        latitudeView = findViewById(R.id.latitude_textView);
+        longitudeView = findViewById(R.id.longitude_textView);
+        accuracyView = findViewById(R.id.accuracy_textView);
+        locationTracker = new LocationTracker(this);
 
         trackingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean hasPermission = ActivityCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) ==
-                        PackageManager.PERMISSION_GRANTED;
-                if(hasPermission){
+                if(hasPermissions()){
                     if(tracking){
                         tracking = false;
                         locationTracker.stopTracking();
@@ -63,15 +74,56 @@ public class MainActivity extends AppCompatActivity {
         br = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                //todo
+                String action = intent.getAction();
+                if(action != null){
+                    if(action.equals(getString(R.string.new_location))){
+                        Location location = intent.getParcelableExtra(getString(R.string
+                                .location_intent_key));
+                        if(location != null){
+                            updateLocationView();
+                        }
+                    }
+                    else if(action.equals("no_location")){
+                        new AlertDialog.Builder(context)
+                                .setMessage(R.string.gps_network_not_enabled)
+                                .setPositiveButton(R.string.open_location_settings,
+                                                    new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                                        MainActivity.this.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                                    }
+                                })
+                                        .setNegativeButton(R.string.Cancel,null)
+                                        .show();
+                    }
+                }
+
+
             }
         };
-        locationTracker = new LocationTracker(this);
-        //todo create filter
-        // todo - register a BroadcastReceiver to listen to the LocationTracker different broadcasts
-        //  , and operate on the UI accordingly.
+        IntentFilter filter = new IntentFilter(getString(R.string.new_location));
+        filter.addAction("no_location");
+        this.registerReceiver(br, filter);
+    }
 
-        //todo - check and ask for permissions + handle result
+    private boolean hasPermissions(){
+        return ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void updateLocationView(){
+        LocationInfo location = locationTracker.getLocationInfo();
+        if(location != null){
+            double latitude =  locationTracker.locationInfo.getLatitude();
+            double longitude = locationTracker.locationInfo.getLongitude();
+            float accuracy = locationTracker.locationInfo.getAccuracy();
+
+            latitudeView.setText(getString(R.string.latitude) + latitude);
+            longitudeView.setText(getString(R.string.longitude) + longitude);
+            accuracyView.setText(getString(R.string.accuracy) + accuracy);
+        }
     }
 
 
@@ -80,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
                                            @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(requestCode == REQUEST_CODE_PERMISSION_LOCATION){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 trackingButton.setText(getString(R.string.stop_tracking));
                 locationTracker.startTracking();
             }
@@ -98,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        //todo - remove the br registration in onDestroy()
+        unregisterReceiver(br);
     }
 }
 
